081bbdf16f44c7b0843494286f43a9524616a26a
yeosiowvic <victoryeo1974@gmail.com>
2023-01-12 11:45:35 +0800

    rename to Swap from swap

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
-import Swap from '../components/Swap/swap';
+import Swap from '../components/Swap/Swap';
 
 export default function Home() {
   return (
