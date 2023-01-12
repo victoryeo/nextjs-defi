f42c0a5dcb860b95b0d5e7e875472dc45f294ed2
yeosiowvic <victoryeo1974@gmail.com>
2023-01-12 13:40:36 +0800

    remove avatar

--- components/Wallet/ConnectWallet.tsx
+++ components/Wallet/ConnectWallet.tsx
@@ -17,7 +17,6 @@ export default function ConnectWallet() {
 
   useEffect(() => {
     if (wallet?.provider) {
-      const { name, avatar } = wallet?.accounts[0].ens ?? {}
       setAccount({
         address: wallet.accounts[0].address,
         balance: wallet.accounts[0].balance
