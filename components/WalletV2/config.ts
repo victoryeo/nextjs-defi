fe56d7f8bd230463b80424a34f8781051f70ece5
yeosiowvic <victoryeo1974@gmail.com>
2023-01-12 20:40:52 +0800

    add wallet V2

---
+++ components/WalletV2/config.ts
@@ -0,0 +1,7 @@
+export const organizationId = process.env.REACT_APP_ORGANIZATION_ID;
+export const collectionId = process.env.REACT_APP_COLLECTION_ID;
+export const networkId = process.env.REACT_APP_NETWORK_ID;
+export const networkName = process.env.REACT_APP_NETWORK_NAME;
+
+export const bncDappId = process.env.REACT_APP_BNC_DAPP_ID;
+export const infuraKey = process.env.REACT_APP_INFURA_KEY;
