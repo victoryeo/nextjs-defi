06c548498807ac93558d80e4a9e0b48b81a8a9fa
yeosiowvic <victoryeo1974@gmail.com>
2023-01-28 21:48:19 +0800

    add WETH gateway address

--- config/constants/contracts.ts
+++ config/constants/contracts.ts
@@ -3,6 +3,9 @@ const contracts = {
   LENDING_POOL: {
     5: '0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6',
   },
+  WETH_GATEWAY: {
+    5: '0xd5B55D3Ed89FDa19124ceB5baB620328287b915d',
+  },
 }
 
 export default contracts
