359002e0b31babbe282dc10e88898dad33a43a6e
yeosiowvic <victoryeo1974@gmail.com>
2023-01-29 11:46:08 +0800

    add supply liquidity for ETH

--- components/Aave/TokenList.tsx
+++ components/Aave/TokenList.tsx
@@ -1,3 +1,8 @@
+import { ethers } from "ethers";
+import { useSelector } from "react-redux";
+import { getWethGwContract } from "../../utils/web3Utils";
+import { selectSigner } from "../../redux/selectors"
+import contracts from "../../config/constants/contracts";
 import styles from "./Aave.module.css";
 
 const tokenList = [
@@ -15,11 +20,29 @@ const tokenList = [
   },
 ];
 
-const handleClick = (token) => {
+let contractWETH: ethers.Contract;
+
+const handleClick = async (token) => {
   alert("supply "+token)
+  const overrides = { gasLimit: 1000000, gasPrice: 210000 };
+  if (token === "DAI") {
+    console.log("supply DAI")
+  } else if (token === "ETH") {
+    console.log("supply ETH")
+    const ethOverrides = { ...overrides, value: 1 };
+    const newSupply = await contractWETH.depositETH(
+      contracts.LENDING_POOL[5],
+      "0x3dfdefe45660999e22344273448535fa8379389377",
+      0, // referralCode
+      ethOverrides
+    );
+  }
 }
 
 export const TokenList = () => {
+  const signer = useSelector(selectSigner);
+  contractWETH = getWethGwContract(signer);
+
   return(
     <div>
       {tokenList.map((e: any) => (
