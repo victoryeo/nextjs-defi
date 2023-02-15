c563cb368d3306e60173feab30c22b515b01caad
yeosiowvic <victoryeo1974@gmail.com>
2023-02-15 14:29:25 +0800

    enable ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import React from "react";
+import { Provider } from "react-redux";
 import { wrapper } from "../store/store";
 import ErrorBoundary from './ErrorBoundary';
 import { setupWallets, WalletProvider } from "../components/WalletV2/WalletConnect";
@@ -8,15 +9,20 @@ import { setupWallets, WalletProvider } from "../components/WalletV2/WalletConne
 setupWallets();
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const { store, props } = wrapper.useWrappedStore(pageProps);
+
   return (
+    <Provider store={store}>
     <ErrorBoundary>
       <WalletProvider>
-        <Component {...pageProps} />
+        <Component {...props} />
       </WalletProvider>
     </ErrorBoundary>
+    </Provider>
   );
 };
 
-export default dynamic(() => Promise.resolve(wrapper.withRedux(App)), {
-  ssr: false,
-});
+export default (App);
+//export default dynamic(() => Promise.resolve(wrapper.withRedux(App)), {
+//  ssr: false,
+//});
