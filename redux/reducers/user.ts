73a5973c14006adf56f06998787d17d9368c08f2
yeosiowvic <victoryeo1974@gmail.com>
2023-02-15 09:54:14 +0800

    add user address to redux

---
+++ redux/reducers/user.ts
@@ -0,0 +1,38 @@
+import { createAction, createSlice } from "@reduxjs/toolkit";
+import { AppState } from "../../store/store";
+import { HYDRATE } from "next-redux-wrapper";
+
+const hydrate = createAction<AppState>(HYDRATE);
+
+// Type for our state
+export interface UserAddress {
+  userAddress: string;
+}
+
+// Initial state
+const initialState: UserAddress = {
+  userAddress: "",
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUserAddress(state, action) {
+      console.log('setUserAddress')
+      state.userAddress = action.payload;
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(hydrate, (state, action) => {
+      return {
+        ...state,
+        ...action.payload.user
+      }
+    })
+  }
+})
+
+export const { setUserAddress } = userSlice.actions
+export const selectUserState = (state: AppState) => state.user.userAddress;
+export default userSlice.reducer
