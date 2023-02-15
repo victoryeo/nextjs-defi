611bb4a016f9ab41f31d8ee8d1f86fa5f3e2f7cf
yeosiowvic <victoryeo1974@gmail.com>
2023-02-15 09:59:32 +0800

    update root select

--- redux/selectors/index.ts
+++ redux/selectors/index.ts
@@ -1,5 +1,5 @@
 import type { AppState } from '../../store/store'
 
-export const selectAuthState = (state: AppState) => state.rootReducer.authState;
-export const selectWeb3Provider = (state: AppState) => state.rootReducer.web3Provider;
-export const selectSigner = (state: AppState) => state.rootReducer.signer;
+export const selectAuthState = (state: AppState) => state.root.authState;
+export const selectWeb3Provider = (state: AppState) => state.root.web3Provider;
+export const selectSigner = (state: AppState) => state.root.signer;
