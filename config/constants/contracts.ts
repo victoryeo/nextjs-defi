4732ba240235fa9a749d239ed84d2875037c73d6
yeosiowvic <victoryeo1974@gmail.com>
2023-02-16 09:01:04 +0800

    add DAI contract address

--- config/constants/contracts.ts
+++ config/constants/contracts.ts
@@ -6,6 +6,9 @@ const contracts = {
   WETH_GATEWAY: {
     5: '0xd5B55D3Ed89FDa19124ceB5baB620328287b915d',
   },
+  DAI_CONTRACT: {
+    5: '0xDF1742fE5b0bFc12331D8EAec6b478DfDbD31464',
+  },
 }
 
 export default contracts
