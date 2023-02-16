ee2df58e0a5d8e574a8d8289fff7fbceb1247573
yeosiowvic <victoryeo1974@gmail.com>
2023-02-16 09:03:19 +0800

    add getDaiContract

--- utils/web3Utils.ts
+++ utils/web3Utils.ts
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers'
 import contracts from '../config/constants/contracts'
 import { weth_gw_abi } from '../config/abi/WethGateway'
+import { erc20_abi } from '../config/abi/ERC20'
 
 const getContract = (abi: any, address: string, signer: ethers.providers.Provider | ethers.Signer) => {
   const contract = new ethers.Contract(address, abi, signer)
@@ -10,4 +11,9 @@ const getContract = (abi: any, address: string, signer: ethers.providers.Provide
 export const getWethGwContract = (signer: ethers.providers.Provider | ethers.Signer): ethers.Contract => {
   console.log("getWethGwContract", signer)
   return getContract(weth_gw_abi, contracts.WETH_GATEWAY[5], signer)
+}
+
+export const getDaiContract = (signer: ethers.providers.Provider | ethers.Signer) => {
+  console.log('getDaiContract', signer)
+  return getContract(erc20_abi, contracts.DAI_CONTRACT[5], signer)
 }
