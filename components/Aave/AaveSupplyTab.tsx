d6be9cfd4271d4c5340a919f1cf7ad8bf0449dc8
yeosiowvic <victoryeo1974@gmail.com>
2023-02-18 10:53:10 +0800

    use mui material table

--- components/Aave/AaveSupplyTab.tsx
+++ components/Aave/AaveSupplyTab.tsx
@@ -1,14 +1,28 @@
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+  Button,
+} from "@mui/material";
 import styles from "./Aave.module.css";
 
 export const AaveSupplyTab = ({ listTab, setStatusFilter, status }: any) => {
   return(
-    <div className={styles.tokenHeader}>
-      {listTab.map((e: any) => (
-        <span className={styles.aavespan}
-          key={e.header}>
-          {e.header}
-        </span>
-      ))}
-    </div>
+    <TableContainer>
+    <Table>
+        <TableHead className={styles.tokenHeader}>
+          <TableRow>
+            {listTab.map((x, index) => (
+              <TableCell key={index}>{x.header}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+    </Table>
+    </TableContainer>
+
   )
 }
