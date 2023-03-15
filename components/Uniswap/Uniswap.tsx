c47d34441bdc0c8568d303f5c7d4320309be01ee
yeosiowvic <victoryeo1974@gmail.com>
2023-03-15 19:55:38 +0800

    add fromValue input box

--- components/Uniswap/Uniswap.tsx
+++ components/Uniswap/Uniswap.tsx
@@ -2,9 +2,29 @@ import React, { useEffect, useState } from "react";
 import styles from "./Uniswap.module.css"
 
 export default function Uniswap() {
+  const [fromValue, setFromValue] = useState<string>("");
+  const [toValue, setToValue] = useState<string>("")
+  
   return (
     <div className={styles.container}>
-      <div className={styles.title}>UNISWAP API</div>
+      <div  className={styles.title}>UNISWAP API</div>
+      <div className={styles.box}>
+        <div className={styles.fromAsset}>
+          <input
+            className={styles.input}
+            type="number"
+            placeholder="0"
+            value={fromValue}
+            onChange={(e) => setFromValue(e.target.value)}
+            onBlur={(e) => {
+              if (e.target.value === "") {
+                setToValue("0");
+                return console.log("NULL");
+              }
+            }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
