323dd1aa566f800e180cbebff52de56070f0afbf
yeosiowvic <victoryeo1974@gmail.com>
2023-03-15 19:22:43 +0800

    add uniswap css

--- components/Uniswap/Uniswap.tsx
+++ components/Uniswap/Uniswap.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import styles from "./Uniswap.module.css"
 
 export default function Uniswap() {
   return (
-    <></>
+    <div className={styles.container}>
+      <div className={styles.title}>UNISWAP API</div>
+    </div>
   )
 }
